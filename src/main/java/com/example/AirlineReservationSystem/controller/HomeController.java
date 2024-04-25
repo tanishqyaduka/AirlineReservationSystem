d148b3c8a37e71a0390d610d844e1a043edb9317
Tanishq Yaduka <prabhatkyaduka@rediffmail.com>
2024-04-25 12:37:41 +0530

    Final Commit

--- src/main/java/com/example/AirlineReservationSystem/controller/HomeController.java
+++ src/main/java/com/example/AirlineReservationSystem/controller/HomeController.java
@@ -10,4 +10,64 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/updateFlights")
+    public String updateFlight() {
+        return "addUpdateFlights";
+    }
+
+    @GetMapping("/cancelReservation")
+    public String cancelRes() {
+        return "cancelReservation";
+    }
+
+    @GetMapping("/createPassenger")
+    public String createPass() {
+        return "createPassenger";
+    }
+
+    @GetMapping("/createReservation")
+    public String createRes() {
+        return "createReservation";
+    }
+
+    @GetMapping("/deleteFlight")
+    public String delFlight() {
+        return "deleteFlight";
+    }
+
+    @GetMapping("/deletePassenger")
+    public String delPass() {
+        return "deletePassenger";
+    }
+
+    @GetMapping("/getPassenger")
+    public String getPassDeets() {
+        return "getPassengerDetails";
+    }
+
+    @GetMapping("/getReservation")
+    public String getRes() {
+        return "getReservation";
+    }
+
+    @GetMapping("/displayFlights")
+    public String dispFlights() {
+        return "showAllFlights";
+    }
+
+    @GetMapping("/dispPartFlight")
+    public String dispPart() {
+        return "showParticularFlightDetails";
+    }
+
+    @GetMapping("/updatePassenger")
+    public String updatepass() {
+        return "updatePassenger";
+    }
+
+    @GetMapping("/updateReservation")
+    public String updateRes() {
+        return "updateReservation";
+    }
 }
